77345250edc91bde42ba30b767ad11efc4065757
simal-nasir <simalnasir1@gmail.com>
2023-10-02 16:42:11 +0500

    string

--- 2-oct-2023/Function.py
+++ 2-oct-2023/Function.py
@@ -1,5 +1,9 @@
+b=1
 def calculate_tax(bill,tax_rate):
     a= (bill * tax_rate) / 100.00
     print('Total Tax:',a)
+    print(b)
+    c=2
     return a
 calculate_tax(170.00,15)
+
---
+++ 2-oct-2023/SliceOfString.py
@@ -0,0 +1,3 @@
+message= "A kong string with a silly typo"
+message= message[0:2] + "l"+ message[3:]
+print(message)
