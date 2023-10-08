86345ba04a95571983bb3833acecece6b5e76eac
simal-nasir <simalnasir1@gmail.com>
2023-10-08 19:34:26 +0500

    A1

---
+++ Assignment 1/Q1.py
@@ -0,0 +1,5 @@
+# 1. Under the num_list create a new for loop and print out each value on the list in sequential order
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+num=sorted(num_list)
+for n in num:
+    print(n)
