bcf03bf6a49cf962d57c7b8689267d9259d91273
simal-nasir <simalnasir1@gmail.com>
2023-10-08 19:39:57 +0500

    Q2

---
+++ Assignment 1/Q2.py
@@ -0,0 +1,6 @@
+# 2.Inside the for loop, create a condition that will look for all numbers that are greater than 45
+# and print out only numbers that meet that condition.
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+for n in num_list:
+    if(n > 45):
+        print(n)
