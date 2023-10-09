07826f0e9d9c167d48b4068586daad0a55d00b34
simal-nasir <simalnasir1@gmail.com>
2023-10-09 16:04:23 +0500

    new

---
+++ 9-oct-2023/format.py
@@ -0,0 +1,3 @@
+name=input("Enter your name").strip()
+number=len(name.strip())*3
+print("Hello {name},your lucky number is {number}".format(name=name,number=number))
---
+++ 9-oct-2023/format2.py
@@ -0,0 +1,4 @@
+price= 6.5
+with_tax= price * 1.09
+print(price,with_tax)
+print("Base price: ${:.2f}. With tax: ${:.2f}".format(price,with_tax))
