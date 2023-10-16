7f2d99cbd46fcb3a7e34fb3b39a815339947b797
simal-nasir <simalnasir1@gmail.com>
2023-10-16 14:32:49 +0500

    lab

---
+++ 16-oct-2023/enumerate.py
@@ -0,0 +1,7 @@
+a= ["eat","sleep","repeat"]
+b= "geek"
+obj1 = enumerate(a)
+obj2 = enumerate(b)
+print("Return type:", type(obj1))
+print(list(enumerate(a,1)))
+print(list(enumerate(b,3)))
--- 9-oct-2023/tuples.py
+++ 9-oct-2023/tuples.py
@@ -3,6 +3,5 @@ def convert_seconds(seconds):
     minutes = (seconds-hours * 3600) // 60
     remaining_seconds = seconds - hours * 3600 -minutes *60
     return hours , minutes ,remaining_seconds
-result = convert_seconds(5000)
-print(result)
-print(type(result))
+hours , minutes ,remaining_seconds= convert_seconds(5000)
+print(minutes)
